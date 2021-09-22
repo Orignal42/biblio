086e0f911b78e0ba289e042fc1141d6c579b6df8
Orignal42 <rbarriquand@gmail.com>
2021-09-22 16:51:57 +0200

    22/09 16:51

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Library;
 
 
 class HomeController extends AbstractController
@@ -13,9 +14,15 @@ class HomeController extends AbstractController
      * @Route("/", name="home")
      */
     public function index(): Response
-    {
+    {    $library = $this->getDoctrine()
+        ->getRepository(Library::class)
+        ->findAll();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'library' => $library,
         ]);
     }
 }
+
+
+
